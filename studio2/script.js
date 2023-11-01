58086eba3f0063bd7bf2da77757af288704304d2
emharo20 <emsharo@ucdavis.edu>
2023-11-01 15:48:00 -0700

    tried to add some animation but I have no clue what I'm doing

--- studio2/script.js
+++ studio2/script.js
@@ -1,3 +1,22 @@
 (function(){
     'use strict';
+
+    const hotSpots = document.querySelectorAll('#container div');
+    const theImg = document.querySelector('div img');
+
+    hotSpots.forEach(function(eachSpot){
+        eachSpot.addEventListener('mouseover', zoomPhoto);
+        eachSpot.addEventListener('mouseout', function(){
+            theImg.className = "start";
+        });
+    });
+
+    function zoomPhoto(event){
+        const thisCorner = event.target.id;
+        //console.log(thisCorner);
+
+        switch(thisCorner){
+            case 'shadow': theImg.className = 'shadow'; break;
+        }
+    }
 })();
