1dee5001bb4f06b2fe5d79ae160cc10a71c62c06
emharo20 <emsharo@ucdavis.edu>
2023-11-08 15:47:42 -0800

    adding more to every picture project

--- studio2/script.js
+++ studio2/script.js
@@ -3,22 +3,28 @@
 
     const hotSpots = document.querySelectorAll('#container div');
     const theImg = document.querySelector('div img');
+    const theContainer = document.querySelector('#container');
 
     hotSpots.forEach(function(eachSpot){
-        eachSpot.addEventListener('mouseover', zoomPhoto);
-        eachSpot.addEventListener('mouseout', function(){
-            theImg.className = "start";
-        });
+        eachSpot.addEventListener('click', zoomPhoto);
     });
 
     function zoomPhoto(event){
         const thisCorner = event.target.id;
-        //console.log(thisCorner);
 
         switch(thisCorner){
             case 'shadow': theImg.className = 'shadow'; break;
             case 'flyaways': theImg.className = 'flyaways'; break;
             case 'current': theImg.className = 'current'; break;
         }
+
+        const overlay = document.createElement('div');
+        overlay.className = 'overlay';
+        theContainer.append(overlay);
+
+        overlay.addEventListener('click', function(){
+            theImg.className = "start";
+            overlay.remove();
+        });
     }
 })();
