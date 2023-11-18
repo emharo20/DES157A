b2810d8835c998adb4a4f2160735a856ed3fa903
emharo20 <emsharo@ucdavis.edu>
2023-11-17 22:37:15 -0800

    adding more javascript

--- studio3/script.js
+++ studio3/script.js
@@ -4,8 +4,9 @@
     const startGame = document.querySelector('#startgame');
     const gameControl = document.querySelector('#gamecontrol');
     const game = document.querySelector('#game');
-    const score = document.querySelector('#score');
+    const scorePlace = document.querySelector('#score');
     const actionArea = document.querySelector('#actions');
+    const ace = document.querySelector('#ace');
     let clickCount = 0;
 
     const gameData = {
@@ -17,9 +18,9 @@
         hitCard: [],
         value:[0, 0],
         hitValue: 0,
-        cardSum: 0,
         index: 0,
-        gameEnd: 22
+        stand: 0,
+        gameEnd: 21
     };
 
     startGame.addEventListener('click', function(){
@@ -33,32 +34,48 @@
 
     function setUpTurn(){
         game.innerHTML = `Now Playing: ${gameData.players[gameData.index]}`;
-        play21();
+        scorePlace.innerHTML = '';
+        actionArea.innerHTML = '';
+        setTimeout(play21, 3000);
+        //play21();
     };
 
     function play21(){
-        gameData.upCard = Math.floor(Math.random()*13);
+        gameData.upCard = Math.floor(Math.random()*0);
         gameData.downCard = Math.floor(Math.random()*13);
         gameData.value[0] = cardValues(gameData.upCard);
         gameData.value[1] = cardValues(gameData.downCard);
-        gameData.cardSum = gameData.value[0] + gameData.value[1];
+        gameData.score[gameData.index] = gameData.value[0] + gameData.value[1];
         
-        game.innerHTML += `<p>Cards Recieved: ${gameData.deck[gameData.upCard]} ${gameData.deck[gameData.downCard]}</p>`;
-        game.innerHTML += `<p>Total Points: ${gameData.cardSum}</p>`;
+        game.innerHTML = `<p>Cards Recieved: ${gameData.deck[gameData.upCard]} ${gameData.deck[gameData.downCard]}</p>`;
+        scorePlace.innerHTML = `<p>Total Points: ${gameData.score[gameData.index]}</p>`;
 
-        actionArea.innerHTML = '<button id="hit">Hit</button> <button id="stand">stand</button>'
+        actionArea.innerHTML = '<button id="hit">Hit</button> <button id="stand">stand</button>';
 
-        //console.log(gameData.hitCard);
         document.querySelector('#hit').addEventListener('click', function(){
             hit();
             game.innerHTML = `<p>Cards Recieved: ${gameData.deck[gameData.upCard]} ${gameData.deck[gameData.downCard]}</p>`;
-            //game.innerHTML += `<p>Added Cards: ${gameData.deck[gameData.hitCard[clickCount]]}</p>`;
             game.innerHTML += `<p>Added Cards: ${hitSuit()}</p>`;
-            game.innerHTML += `<p>Total Points: ${gameData.cardSum}</p>`;
+            scorePlace.innerHTML = `<p>Total Points: ${gameData.score[gameData.index]}</p>`;
             clickCount++;
-            //console.log(gameData.hitCard);
+
+            if(gameData.score[gameData.index] > gameData.gameEnd){
+                gameData.stand++;
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                game.innerHTML += `<p>Sorry! You went over 21.</p>`;
+                setTimeout(setUpTurn, 3000);
+            }
         });
-        
+        document.querySelector('#stand').addEventListener('click', function(){
+            gameData.stand++;
+            if(gameData.stand === gameData.players.length){
+                checkWinningCondition();
+            }
+            else if(gameData.stand < gameData.players.length){
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                setUpTurn();
+            }
+        });
     };
 
     function cardValues(number){
@@ -68,7 +85,7 @@
             value = 10;
         }
         else if(gameData.deck[number] == "A"){
-            value = 1;
+            value = valueA();
         }
         else {
             value = parseInt(gameData.deck[number]);
@@ -77,10 +94,25 @@
         return value;
     }
 
+    function valueA(){
+        let a = 0;
+        ace.innerHTML = '<p>Value for A: <button id="one">1</button> or <button id="eleven">11</button></p>' 
+        document.querySelector('#one').addEventListener('click', function(){
+            a = 1;
+            ace.innerHTML = '';
+            return a;
+        });
+        document.querySelector('#eleven').addEventListener('click', function(){
+            a = 11;
+            ace.innerHTML = '';
+            return a;
+        });
+    }
+
     function hit(){
         gameData.hitCard.push(Math.floor(Math.random()*13));
         gameData.hitValue = cardValues(gameData.hitCard[clickCount]);
-        gameData.cardSum = gameData.cardSum + gameData.hitValue;
+        gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.hitValue;
     }
 
     function hitSuit(){
@@ -93,4 +125,18 @@
         return hitCardSuit;
     }
     
+    function checkWinningCondition(){
+        scorePlace.innerHTML = `<p>${gameData.players[0]} score: <strong>${gameData.score[0]}</strong></p>`;
+        scorePlace.innerHTML += `<p>${gameData.players[1]} score: <strong>${gameData.score[1]}</strong></p>`;
+        game.innerHTML = '';
+        actionArea.innerHTML = '';
+        document.querySelector('#quit').innerHTML = "Start a new game";
+
+        /* if(gameData.score[gameData.index[0]] == gameData.score[gameData.index[1]] == gameData.gameEnd){
+            game.innerHTML = `<h2>It's a Tie!</h2>`;
+        }
+        if(gameData.score[gameData.index] < gameData.gameEnd){
+            game.innerHTML = `<h2>${gameData.players[gameData.index]} won!</h2>`;
+        } */
+    }
 })();
